6e7dd65ccec5fac69f11be72ee4c9a7871633e38
abequinonez <abequinonez@users.noreply.github.com>
2018-04-14 01:05:15 -0700

    feat: Modify Passport strategies to persist data

--- passport-init.js
+++ passport-init.js
@@ -1,19 +1,24 @@
 const LocalStrategy = require('passport-local').Strategy;
 const bcrypt = require('bcryptjs');
 
-// Temporary data store
-const users = {};
+// Import the User model
+const { User } = require('./models/user');
 
 module.exports = function(passport) {
   // Serialize the authenticated user to the session
-  passport.serializeUser((user, done) => {
-    console.log('Serializing user:', user.username);
-    return done(null, user.username);
-  });
+  passport.serializeUser((user, done) => done(null, user._id));
 
   // Deserialize the user for subsequent requests
-  passport.deserializeUser((username, done) => {
-    return done(null, users[username]);
+  passport.deserializeUser((id, done) => {
+    User.findById(id)
+      .then(user => {
+        if (!user) {
+          return done(null, false);
+        }
+
+        return done(null, user);
+      })
+      .catch(err => done(err, false));
   });
 
   passport.use(
@@ -22,21 +27,27 @@ module.exports = function(passport) {
       { passReqToCallback: true },
       (req, username, password, done) => {
         // First check if the user already exists
-        if (users[username]) {
-          return done(
-            null,
-            false,
-            req.flash('error', 'Username already taken')
-          );
-        }
+        User.findOne({ username })
+          .then(user => {
+            // If user exists, return an error
+            if (user) {
+              return done(
+                null,
+                false,
+                req.flash('error', 'Username already taken')
+              );
+            }
 
-        // Save user to the DB
-        users[username] = {
-          username,
-          password: hashPassword(password)
-        };
+            // Otherwise, create a new user
+            const newUser = new User({
+              username,
+              password: hashPassword(password)
+            });
 
-        return done(null, users[username]);
+            // Save user to the DB and return new user object
+            return newUser.save().then(user => done(null, user));
+          })
+          .catch(err => done(err, false));
       }
     )
   );
@@ -46,12 +57,20 @@ module.exports = function(passport) {
     new LocalStrategy(
       { passReqToCallback: true },
       (req, username, password, done) => {
-        // Check if user exists and if password is correct
-        if (!users[username] || !isValidPassword(users[username], password)) {
-          return done(null, false, req.flash('error', 'Invalid credentials'));
-        }
+        User.findOne({ username })
+          .then(user => {
+            // Check if user exists and if password is correct
+            if (!user || !isValidPassword(user, password)) {
+              return done(
+                null,
+                false,
+                req.flash('error', 'Invalid credentials')
+              );
+            }
 
-        return done(null, users[username]);
+            return done(null, user);
+          })
+          .catch(err => done(err, false));
       }
     )
   );
